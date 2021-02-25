981beafa54cdbc1057f075e14ba0f001c0b78731
Matia <matia.casu@gmail.com>
2021-02-25 14:57:27 +0100

    Creati prompt e generata password

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var nome = prompt("Ciao! Qual è il tuo nome?");
+var cognome = prompt("Ora digita il tuo cognome!");
+var colore = prompt("E il tuo colore preferito?");
+
+var nuovaPassword = nome + cognome + colore + "21";
+document.write("La tua nuova password è: " + nuovaPassword);
